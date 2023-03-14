3ccd423bc5251e643da4423c0dd7fa394c0a1845
Jay <jaywerra@gmail.com>
2023-03-14 14:23:47 -0700

    Cleanup

--- src/scripts/createPdf.js
+++ src/scripts/createPdf.js
@@ -1,43 +1,21 @@
-import html2canvas from "html2canvas";
 import { jsPDF } from "jspdf";
-const printButton = document.querySelector(".button");
-const elementToPrint = document.querySelector(".container");
 import { saveAs } from "file-saver";
 import domtoimage from 'dom-to-image';
-import domToPdf from "dom-to-pdf";
-
-function createPDF() {
-    html2canvas(elementToPrint, {
-        allowTaint: true,
-        backgroundColor: "#faf2e8",
-        foreignObjectRendering: true,
-        letterRendering: 1,
-        logging: true,
-        useCORS: true
-    }).then(function (canvas) {
-        document.body.appendChild(canvas);
-        let imgdata = canvas.toDataURL("image/jpeg");
-        let doc = new jsPDF();
-
-        console.log("IMG", imgdata);
-        doc.addImage(imgdata, "JPG", 0, 0, elementToPrint.offsetWidth, elementToPrint.offsetHeight);
-        // doc.save("map.pdf");
-    });
-}
-
-
+const printButton = document.querySelector(".button");
+const elementToPrint = document.querySelector(".container");
 
 function downloadMap () {
     domtoimage.toPng(elementToPrint, {
         style: {
             backgroundColor: "#00ffff",
+            margin: 0,
         },
     }).then(dataUrl => {
       let htmlImage = new Image();
-      let pdf = new jsPDF('l', 'px', [725, 585]);
+      let pdf = new jsPDF('l', 'px', [500, 400]);
 
       htmlImage.src = dataUrl;
-      pdf.addImage(htmlImage, 0, 0, 725, 585);
+      pdf.addImage(htmlImage, 0, 0, 500, 400);
       pdf.save("map.pdf");
 
     }).catch(error => {
