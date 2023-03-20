f63133f61889369f461b4cb66be8aecc8d07388e
Jay <jaywerra@gmail.com>
2023-03-20 09:52:40 -0700

    removing canvas package

--- src/scripts/createPdf.js
+++ src/scripts/createPdf.js
@@ -1,32 +1,8 @@
-import html2canvas from 'html2canvas';
 import { jsPDF } from "jspdf";
 import domtoimage from 'dom-to-image';
 
 const printButton = document.querySelector(".button");
 const elementToPrint = document.querySelector(".container");
-
-function printPDF () {
-    html2canvas(elementToPrint, { onclone: (document) => {
-        printButton.style.display = "none"
-    }})
-    .then(canvas => {
-        const img = canvas.toDataURL('image/png')
-        const pdf = new jsPDF({
-            orientation: 'landscape',
-            unit: 'mm',
-            format: 'a4',
-            putOnlyUsedFonts: true,
-            backgroundColor: "#faf2e8",
-        });
-        let imgData = new Image();
-
-        let width = pdf.internal.pageSize.getWidth();
-        let height = pdf.internal.pageSize.getHeight();
-
-        pdf.addImage(img, 0, 0, width, height);
-        pdf.save('map.pdf')
-    })
-}
 
 function downloadMap () {
     domtoimage.toPng(elementToPrint, {
